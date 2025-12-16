03b24a554cd41c7c186ff836798be6c8836ed926
devillove0008-code <devillove0008@gmail.com>
2025-12-16 20:51:14 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+API_ID = 30753606               # my.telegram.org
+API_HASH = "f17efd0272ebde7aac236053c1a36827"
+BOT_TOKEN = "8218645672:AAGQzQCfGV8ygZXPg8wZVwECrdSyV8T-dW8"
+
+BOT_USERNAME = "DevilMuzicbot"
+
+OWNER_URL = "https://t.me/your_owner"
+CHANNEL_URL = "https://t.me/your_channel"
+SUPPORT_URL = "https://t.me/your_support"
+
+DOWNLOAD_DIR = "downloads"
